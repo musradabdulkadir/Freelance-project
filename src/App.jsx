47aa45f5b33c68df082714f099006a2c64da23be
Musrad <musradabdul007@gmail.com>
2026-07-20 20:05:48 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <>
-      <h1>Freelance Platform</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
