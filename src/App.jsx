6c495a46367e6a31b37c8a64eac9e712d3ef2820
Musrad <musradabdul007@gmail.com>
2026-07-20 21:23:10 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import MainLayout from "./layouts/MainLayout";
+import About from "./pages/About";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Route>
     </Routes>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,27 @@
-
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="bg-amber-100 p-3 flex justify-between">
+      <div>
+        <p className="text-4xl font-bold outfit">SkillBridge</p>
+      </div>
+      <div className="items-center justify-center outfit">
+        <a className="ml-3 mr-3 text-lg" href="#">
+          Home
+        </a>
+        <a className="ml-3 mr-3 text-lg" href="#">
+          Job Finds
+        </a>
+        <a className="ml-3 mr-3 text-lg" href="#">
+          Freelance
+        </a>
+        <a className="ml-3 mr-3 text-lg" href="#">
+          About
+        </a>
+      </div>
+      <div>
+        <button className="bg-black text-white px-4 py-1 text-lg rounded-3xl mr-3 outfit">Login</button>
+        <button className="bg-black text-white px-4 py-1 text-lg rounded-3xl mr-3 outfit">Register</button>
+      </div>
+    </div>
+  );
 }
