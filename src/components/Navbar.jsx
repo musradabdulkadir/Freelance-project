3deddfad444745981ee0017377d1077320f60acd
Musrad <musradabdul007@gmail.com>
2026-07-20 23:25:32 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,56 @@
+import { NavLink } from "react-router-dom";
+
+const links = [
+  {
+    path: "/",
+    text: "Home",
+  },
+  {
+    path: "/about",
+    text: "About",
+  },
+  {
+    path: "/findjobs",
+    text: "Find Jobs",
+  },
+  {
+    path: "/freelancers",
+    text: "Freelancers",
+  },
+];
 export default function Navbar() {
   return (
-    <div className="bg-amber-100 p-3 flex justify-between">
+    <nav className="bg-amber-100 p-3 flex items-center justify-between">
       <div>
         <p className="text-4xl font-bold outfit">SkillBridge</p>
       </div>
-      <div className="items-center justify-center outfit">
-        <a className="ml-3 mr-3 text-lg" href="#">
-          Home
-        </a>
-        <a className="ml-3 mr-3 text-lg" href="#">
-          Job Finds
-        </a>
-        <a className="ml-3 mr-3 text-lg" href="#">
-          Freelance
-        </a>
-        <a className="ml-3 mr-3 text-lg" href="#">
-          About
-        </a>
+
+      <div className="flex items-center justify-center gap-6 outfit">
+        {links.map((link) => (
+          <NavLink
+            className={({ isActive }) =>
+              `text-lg ${
+                isActive
+                  ? "font-bold text-black underline underline-offset-4"
+                  : "text-gray-600"
+              } `
+            }
+            to={link.path}
+            key={link.path}
+          >
+            {link.text}
+          </NavLink>
+        ))}
       </div>
+
       <div>
-        <button className="bg-black text-white px-4 py-1 text-lg rounded-3xl mr-3 outfit">Login</button>
-        <button className="bg-black text-white px-4 py-1 text-lg rounded-3xl mr-3 outfit">Register</button>
+        <button className="bg-black text-white px-4 py-1 text-lg rounded-3xl mr-3 outfit">
+          Login
+        </button>
+        <button className="bg-black text-white px-4 py-1 text-lg rounded-3xl mr-3 outfit">
+          Register
+        </button>
       </div>
-    </div>
+    </nav>
   );
 }
