f3c32ed726722d280f2e99a663becc3278f68364
Musrad <musradabdul007@gmail.com>
2026-07-22 15:00:27 +0530

    Create

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import CallToAction from "../components/home/CallToAction";
 import Categories from "../components/home/Categories";
-import FeaturedJobs from "../components/home/featuredJobs";
+import FeaturedJobs from "../components/home/FeaturedJobs";
 import Hero from "../components/home/Hero";
 import HowItWorks from "../components/home/HowItsWorks";
 import Testimonials from "../components/home/Testimonials";
